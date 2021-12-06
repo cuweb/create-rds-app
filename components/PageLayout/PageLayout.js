c9d1f1e12d0f5d57f45b1e49375e4003c3cccf38
Marcelo <glacial@gmail.com>
2021-12-06 13:34:25 -0500

    initial structure

---
+++ components/PageLayout/PageLayout.js
@@ -0,0 +1,34 @@
+import { Layout, Aside, Main, Sidebar, Banner } from '@marceloglacial/rds-beta';
+
+const PageLayout = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <header>
+        <Banner title='Welcome to RDS' />
+      </header>
+      <Layout type='ama'>
+        <Aside>
+          <h3>Left Aside</h3>
+          <p>
+            The left-side aside is intended for site navigation using the Menu
+            block. The column has a fixed width of 200px, and disappears from
+            view below 810px. While the main purposes of this aside is to
+            contain a site navigation, additional aside blocks can be added
+            below the menu.
+          </p>
+        </Aside>
+        <Main>{children}</Main>
+        <Sidebar>
+          <h3>Right aside</h3>
+          <p>
+            The right-side aside is intended for providing content related to
+            what is dispplayed in the main area. The column has a fixed width of
+            270px, and drops below the main content area below 810px.
+          </p>
+        </Sidebar>
+      </Layout>
+    </>
+  );
+};
+export default PageLayout;
