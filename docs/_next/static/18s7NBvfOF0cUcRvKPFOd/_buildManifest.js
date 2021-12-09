f0740cfda094d1d1adb6f200aea9802e862a176b
Marcelo <glacial@gmail.com>
2021-12-09 12:22:51 -0500

    build

--- docs/_next/static/18s7NBvfOF0cUcRvKPFOd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/40-a308b185a3d4a922.js","static/chunks/pages/index-f04753e65612929c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
