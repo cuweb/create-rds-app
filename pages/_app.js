3e10c989de61afa1aa654e211556d24ab4fe6890
marceloglacial <glacial@gmail.com>
2022-01-28 11:39:56 -0500

    add prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../node_modules/@marceloglacial/rds-beta/dist/rds.css';
+import '../node_modules/@marceloglacial/rds-beta/dist/rds.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,28 @@
-import { AlertHeader, Masthead, TextImage } from '@marceloglacial/rds-beta';
-import { useState } from 'react';
-import PageLayout from '../components/PageLayout/PageLayout';
+import { Masthead, TextImage } from '@marceloglacial/rds-beta'
+import { useState } from 'react'
+import PageLayout from '../components/PageLayout/PageLayout'
 
 export default function Home() {
-  const [isHidden, setIsHidden] = useState(false);
+    const [isHidden, setIsHidden] = useState(false)
 
-  const handleClose = () => {
-    setIsHidden(true);
-  };
+    const handleClose = () => {
+        setIsHidden(true)
+    }
 
-  return (
-    <>
-      <Masthead title='Ravens Design System' />
-      <AlertHeader
-        description='Due to inclement weather the university will be closed January 28, 2019. Please see the <a href="https://carleton.ca/ses/exam-schedule/">updated exam schedule</a>.'
-        handleClose={() => handleClose(false)}
-        isHidden={isHidden}
-        title='University closure, January 28, 2021!'
-      />
-      <PageLayout>
-        <TextImage
-          content='It is important to note the text is not intended to wrap around the image. In general, the amount of text used should be the same height as the image on large screens. This will, of course, differ if you use a large or small block size.'
-          direction='right'
-          image={{
-            alt: 'image',
-            src: 'https://via.placeholder.com/640x480',
-          }}
-          title='Image to the Right'
-        />
-      </PageLayout>
-    </>
-  );
+    return (
+        <>
+            <Masthead title='Ravens Design System' />
+            <PageLayout>
+                <TextImage
+                    content='It is important to note the text is not intended to wrap around the image. In general, the amount of text used should be the same height as the image on large screens. This will, of course, differ if you use a large or small block size.'
+                    direction='right'
+                    image={{
+                        alt: 'image',
+                        src: 'https://via.placeholder.com/640x480',
+                    }}
+                    title='Image to the Right'
+                />
+            </PageLayout>
+        </>
+    )
 }
