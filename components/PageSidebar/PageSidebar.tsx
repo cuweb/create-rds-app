3740b557432f7fe74bfc7c73c3761d38e84a7d60
marceloglacial <glacial@gmail.com>
2022-01-28 16:54:01 -0500

    add children to sidebar

--- components/PageSidebar/PageSidebar.tsx
+++ components/PageSidebar/PageSidebar.tsx
@@ -1,8 +1,10 @@
 const PageSideBar: React.FC = (props) => {
+    const { children } = props
     return (
         <section>
             <h3>Sidebar</h3>
             <p>Future content</p>
+            {children}
         </section>
     )
 }
