be382c9b41902fa3fdab7afc25c6e43f3832b0a0
marceloglacial <glacial@gmail.com>
2022-01-28 16:45:39 -0500

    update info

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,15 @@ export default function Home() {
                 <a href='#'>Menu Item</a>
             </Masthead>
             <PageLayout {...siteInfo}>
-                <h2>Content</h2>
+                <p>
+                    Carleton&apos;s Raven Design System (RDS) was built to help
+                    deliver a unified experience across our many digital
+                    properties. By establishing a harmonious, single source of
+                    truth for our design decisions and development guidelines,
+                    RDS increases the efficiency campus-wide teams are able to
+                    build consistent, on-brand, and user-focused websites and
+                    applications.
+                </p>
             </PageLayout>
         </>
     )
