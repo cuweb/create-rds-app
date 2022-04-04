c0afbc62070ea1d06d2ecd76af1f813f33b620e7
marceloglacial <glacial@gmail.com>
2022-04-04 15:17:57 -0400

    update layout and login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,12 @@
 import siteInfo from 'data/siteinfo.json'
-import { Masthead } from '@marceloglacial/rds-beta'
+import { Ublock } from '@marceloglacial/rds-beta'
 import PageLayout from 'components/PageLayout/PageLayout'
-import { useSession } from 'next-auth/react'
+import { FC } from 'react'
 
-export default function Home() {
-    const { data: session } = useSession()
-    const welcome = session
-        ? `Welcome ${
-              session?.user?.name?.split('\\')[1] || session?.user?.name
-          }`
-        : 'Welcome guest. Please sign in'
-    const actions = {
-        buttons: [
-            {
-                title: welcome,
-            },
-            {
-                title: session ? 'Sign Out' : 'Sign In',
-                link: session?.user ? '/api/auth/signout' : '/api/auth/signin',
-                color: 'red',
-            },
-        ],
-    }
-    //console.log({ session })
+const Home: FC = (): JSX.Element => {
     return (
-        <>
-            <Masthead
-                title='Ravens Design System'
-                url={'/'}
-                menu={[
-                    {
-                        title: 'Item 1',
-                        linl: '#',
-                    },
-                    {
-                        title: 'Item 2',
-                        linl: '#',
-                    },
-                ]}
-                actions={actions}
-            />
-            <PageLayout {...siteInfo}>
+        <PageLayout {...siteInfo}>
+            <Ublock>
                 <p>
                     Carleton&apos;s Raven Design System (RDS) was built to help
                     deliver a unified experience across our many digital
@@ -50,7 +16,8 @@ export default function Home() {
                     build consistent, on-brand, and user-focused websites and
                     applications.
                 </p>
-            </PageLayout>
-        </>
+            </Ublock>
+        </PageLayout>
     )
 }
+export default Home
